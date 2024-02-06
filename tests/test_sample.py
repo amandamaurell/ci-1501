d57abfd75d75372a1f76dc6630607c7a65a1efa5
Amanda Maurell <amanda.maurell@gmail.com>
2024-02-06 09:34:52 +0100

    testing cd

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,5 +4,5 @@ import unittest
 
 class TestSample(unittest.TestCase):
     def test_sample(self):
-        # We are simply checking whether 42==42!
+        # We are simply checking whether 42==42!!!!
         self.assertEqual(40, 40)
